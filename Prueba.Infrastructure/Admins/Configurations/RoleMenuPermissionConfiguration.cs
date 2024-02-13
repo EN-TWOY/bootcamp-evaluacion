2fed93f3845bcda293e75184d48b1857462f80ea
Neiser <n31dfts@gmail.com>
2024-02-13 09:31:43 -0500

    update

---
+++ Prueba.Infrastructure/Admins/Configurations/RoleMenuPermissionConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Prueba.Domain.Admins.Models;
+using Prueba.Infrastructure.Cores.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba.Infrastructure.Admins.Configurations
+{
+    public class RoleMenuPermissionConfiguration : IEntityTypeConfiguration<RoleMenuPermission>
+    {
+        public void Configure(EntityTypeBuilder<RoleMenuPermission> builder)
+        {
+            builder.ToTable("rolemenupermission", "adm");
+
+            builder.HasKey(t => t.RoleId);
+
+            builder.Property(t => t.MenuId).HasColumnName("menuid");
+            builder.Property(t => t.PermissionId).HasColumnName("permissionid");
+            builder.Property(t => t.RegistrationDate).HasColumnName("registrationdate")
+                .HasConversion(new DateTimeToDateTimeOffset());
+            builder.Property(t => t.State).HasColumnName("state");
+
+            // Relations
+            builder.HasOne(a => a.Permission)
+                .WithMany(at => at.RoleMenuPermissions)
+                .HasForeignKey(a => a.PermissionId);
+        }
+    }
+}
---
+++ Prueba.Infrastructure/Admins/Persistences/RoleMenuPermissionRepository.cs
@@ -0,0 +1,14 @@
+﻿using Prueba.Infrastructure.Cores.Persistences;
+using Prueba.Domain.Admins.Models;
+using Prueba.Domain.Admins.Repositories;
+using Prueba.Infrastructure.Cores.Contexts;
+
+namespace Prueba.Infrastructure.Admins.Persistences
+{
+    public class RoleMenuPermissionRepository : CrudRepository<Permission, int>, IPermissionRepository
+    {
+        public RoleMenuPermissionRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
